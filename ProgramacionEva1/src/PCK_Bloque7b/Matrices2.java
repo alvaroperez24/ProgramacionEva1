051d92e95b359bb108b16972828fb5eeed0ba9dc
Usuario <alvaroperzz24@gmail.com>
2025-12-11 20:36:50 +0100

    Terminado

--- ProgramacionEva1/src/PCK_Bloque7b/Matrices2.java
+++ ProgramacionEva1/src/PCK_Bloque7b/Matrices2.java
@@ -1,80 +1,142 @@
 package PCK_Bloque7b;
 
-import java.util.Scanner;	
+import java.util.Scanner;
 
 public class Matrices2 {
-	
-	 Scanner sc = new Scanner (System.in);
+    
+    Scanner sc = new Scanner(System.in);
 
-	public static void Matrices2 (String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // Para probar el juego directamente
+        Matrices2 juego = new Matrices2();
+        juego.juegobarquitos();
+    }
 
-	}
-		public void ejercicio1 () {
-			char [][] tablero = new char[8][8];
-			int fila = 0;
-			int columna = 0;
-			int intentos = 10;
-			int barcos = 20;
-			
-			System.out.println("Introduzca el valor de la columna");
-			columna = sc.nextInt();
-			
-			System.out.println("Introduzca el valor de la fila");
-			fila = sc.nextInt();
-			
-			for (int i = 0;i<8; i++) {
-				for (int j = 0; j<8; j++) {
-					tablero[i][j] = 'X';
-				}
-				
-				 System.out.print(tablero[i] + " " );
-			}
-			// Los 4 barcos de 1 hueco
-			tablero [0][2] = 'O';
-			tablero [2][2] = 'O';
-			tablero [4][2] = 'O';
-			tablero [6][2] = 'O';
-			
-			// Los 3 barcos de 2 huecos
-			//Barco 1
-			tablero [1][1] = 'O';
-			tablero [1][2] = 'O';
-			
-			// Barco 2
-			tablero [7][6] = 'O';
-			tablero [7][7] = 'O';
-			
-			// Barco3 
-			tablero [4][5] = 'O';
-			tablero [3][2] = 'O';
-			
-			// Los 2 barcos de 3 huecos
-			
-			// Barco 1
-			tablero [0][0] = 'O';
-			tablero [1][0] = 'O';
-			tablero [2][0] = 'O';
-			
-			//Barco 2
-			tablero [6][5] = 'O';
-			tablero [6][6] = 'O';
-			tablero [6][7] = 'O';
-			
-			// El barco de 4 huecos
-			tablero [2][3] = 'O';
-			tablero [2][4] = 'O';
-			tablero [2][5] = 'O';
-			tablero [2][6] = 'O';
-			
-			
-				
-			
-			// Entrada al juego
-			System.out.println("Bienvenido al juego hundir la flota");
-			System.out.println("");
-			// Iniciar el juego
-			
-			
-		}
+    public void juegobarquitos() {
+
+        char[][] tablero = new char[8][8];
+
+        int barcos = 0;
+        int intentos = 10;
+        int fila, columna;
+        int x, y;
+
+        // ====== INICIALIZAR TABLERO ======
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero[i].length; j++) {
+                tablero[i][j] = '-';
+            }
+        }
+
+        System.out.println("Introduce las coordenadas de los barcos:");
+        System.out.println("4 barcos de 1 posición, 3 barcos de 2 posiciones, 2 barcos de 3 posiciones, 1 barco de 4 posiciones.");
+        System.out.println();
+
+        // ====== COLOCAR BARCOS ======
+        colocarBarcos(tablero, 4, 1, barcos);
+        colocarBarcos(tablero, 3, 2, barcos);
+        colocarBarcos(tablero, 2, 3, barcos);
+        colocarBarcos(tablero, 1, 4, barcos);
+
+        saltodepagina();
+        System.out.println("¡COMIENZA EL JUEGO!");
+
+        // ====== JUEGO ======
+        while (intentos > 0 && barcos > 0) {
+
+            System.out.println("\nIntroduce fila y columna (0-7).");
+            System.out.println("Intentos: " + intentos + " | Quedan " + barcos + " partes de barcos.\n");
+
+            System.out.print("Fila: ");
+            fila = sc.nextInt();
+            System.out.print("Columna: ");
+            columna = sc.nextInt();
+
+            if (fila < 0 || fila > 7 || columna < 0 || columna > 7) {
+                System.out.println("Coordenada fuera del tablero.");
+                continue;
+            }
+
+            if (tablero[fila][columna] == 'X' || tablero[fila][columna] == 'T') {
+                System.out.println("Ya habías disparado ahí.");
+                continue;
+            }
+
+            if (tablero[fila][columna] == 'O') {
+                System.out.println("¡TOCADO!");
+                tablero[fila][columna] = 'T';
+                barcos--;
+            } else {
+                System.out.println("Agua...");
+                tablero[fila][columna] = 'X';
+                intentos--;
+            }
+
+            mostrarTablero(tablero, true);
+        }
+
+        System.out.println(barcos == 0 ? "¡GANASTE!" : "PERDISTE.");
+
+        System.out.println("\nEstado final del tablero:");
+        mostrarTablero(tablero, false);
+    }
+
+    // ================================================================
+    // MÉTODO PARA COLOCAR BARCOS
+    // ================================================================
+    private void colocarBarcos(char[][] tablero, int cantidad, int tamano, int barcosColocados) {
+        System.out.println("Coloca " + cantidad + " barcos de " + tamano + " posición(es).");
+
+        for (int i = 0; i < cantidad * tamano; i++) {
+
+            System.out.print("Introduce coordenada x (0-7): ");
+            int x = sc.nextInt();
+            System.out.print("Introduce coordenada y (0-7): ");
+            int y = sc.nextInt();
+
+            if (x < 0 || x > 7 || y < 0 || y > 7) {
+                System.out.println("Coordenada inválida.");
+                i--;
+                continue;
+            }
+
+            if (tablero[x][y] == 'O') {
+                System.out.println("Casilla ocupada.");
+                i--;
+                continue;
+            }
+
+            tablero[x][y] = 'O';
+            barcosColocados++;
+
+            mostrarTablero(tablero, false);
+            System.out.println("Parte " + barcosColocados + " colocada.\n");
+        }
+    }
+
+    // ================================================================
+    // MOSTRAR TABLERO (modo oculto o visible)
+    // ================================================================
+    private void mostrarTablero(char[][] tablero, boolean ocultarBarcos) {
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero[i].length; j++) {
+
+                if (ocultarBarcos && tablero[i][j] == 'O') {
+                    System.out.print("- ");
+                } else {
+                    System.out.print(tablero[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    // ================================================================
+    // SALTO DE PÁGINA
+    // ================================================================
+    public void saltodepagina() {
+        for (int i = 0; i <= 50; i++) {
+            System.out.println();
+        }
+    }
 }
